e60972e69b3532f0cec14438a650e6ad3812b2eb
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-11 11:36:07 +0530

    part time of an employee

--- com/bridgelabz/Employeewagebuilder.java
+++ com/bridgelabz/Employeewagebuilder.java
@@ -7,22 +7,25 @@ public class Employeewagebuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation Program");
       
-        int IS_FULLTIME = 1;
+		int IS_FULLTIME = 1;
+        int IS_PARTTIME = 2;
         int Emp_Rate_PerHr = 20;
-        
+       
         int empHrs = 0;
         int empWage = 0;
         /*
           Using Math.random method
          */
         Random random = new Random();
-        int empCheck = random.nextInt(2);
+        int empCheck = random.nextInt(3);
         if (empCheck == IS_FULLTIME)
             empHrs = 8;
+        else if (empCheck == IS_PARTTIME)
+            empHrs = 4;
         else
             empHrs = 0;
         /*
-          Calculating the daily employee wage assuming wage per hour as 20 and Full day hour as 8
+         Calculating the daily employee wage of an employee
          */
         empWage = Emp_Rate_PerHr * empHrs;
         System.out.println("The daily wage of an employee is : " + empWage);
