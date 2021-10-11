788cbd758de55903c7ed6a783033c34ddf7be2b3
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-11 11:28:53 +0530

    employee attendance

--- com/bridgelabz/Employeewagebuilder.java
+++ com/bridgelabz/Employeewagebuilder.java
@@ -1,10 +1,22 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class Employeewagebuilder {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome To Employee Wage Computation Program");
-	}
-}
+        
+        int IS_FULLTIME = 1;
+        /**
+         * Using Math.random method 
+         */
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if (empCheck == IS_FULLTIME)
+            System.out.println("The Employee is Present");
+        else
+            System.out.println("The Employee is Absent");
 
-	
+    }
+}
