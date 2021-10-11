fcce1da348632f0e26c4c9f7722c1ed683de10a7
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-11 11:24:07 +0530

    Employeewage computation

---
+++ com/bridgelabz/Employeewagebuilder.java
@@ -0,0 +1,10 @@
+package com.bridgelabz;
+
+public class Employeewagebuilder {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome To Employee Wage Computation Program");
+	}
+}
+
+	
