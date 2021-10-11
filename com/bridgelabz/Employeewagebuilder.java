28c449ce89be03c9149e0499055138021c31a56a
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-11 11:50:16 +0530

    using method and classes

--- com/bridgelabz/Employeewagebuilder.java
+++ com/bridgelabz/Employeewagebuilder.java
@@ -3,23 +3,22 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class Employeewagebuilder {
-	// Declaring a constant
+	 // Declaring a constant
     public static final int IS_FULLTIME = 1;
     public static final int IS_PARTTIME = 2;
     public static final int Emp_Rate_PerHr = 20;
     public static final int NO_WORKINGDAYS = 20;
     public static final int MAX_HRS_MONTH = 100;
 
-    public static void main(String[] args) {
-        System.out.println("Welcome To Employee Wage Computation Program");
-        // Declaring a variables
+    public static int computeWage() {
+        // Declaring the variables
         int empHrs = 0;
         int empWage = 0;
         int totalEmpWage = 0;
         int totalEmpHrs = 0;
         int totalWorkingDays = 0;
         /*
-         Using random method 
+          Using random method
          */
         while (totalEmpHrs <= MAX_HRS_MONTH && totalWorkingDays < NO_WORKINGDAYS) {
             totalWorkingDays++;
@@ -32,15 +31,21 @@ public class Employeewagebuilder {
                 case IS_PARTTIME: // PartTime Employee
                     empHrs = 4;
                     break;
-                default: // Employee is absent
+                default:  // Employee is absent
                     empHrs = 0;
             }
             empWage = Emp_Rate_PerHr * empHrs;
             totalEmpHrs += empHrs;
             System.out.println("Day : " + totalWorkingDays + " : Employee worked : " + empHrs + " Hours " +
-                               ", Employee Wage : " + empWage);
+                                ", Employee Wage : " + empWage);
         }
         totalEmpWage = totalEmpHrs * Emp_Rate_PerHr;
-        System.out.println("Total Wages of an Employee is : " + totalEmpWage);
+        System.out.println("Total Wages is : " + totalEmpWage);
+        return totalEmpWage;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Employee Wage Computation Program");
+        computeWage();
     }
 }
