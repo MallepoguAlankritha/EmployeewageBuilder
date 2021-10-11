74d05b00ad8bc33d0c00685c5acb00baece89a7c
MallepoguAlankritha <mallepogualankritha@gmail.com>
2021-10-11 11:40:26 +0530

    using switchcase

--- com/bridgelabz/Employeewagebuilder.java
+++ com/bridgelabz/Employeewagebuilder.java
@@ -3,31 +3,34 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class Employeewagebuilder {
+        public static final int IS_FULLTIME = 1;
+	    public static final int IS_PARTTIME = 2;
+	    public static final int Emp_Rate_PerHr = 20;
 
-	public static void main(String[] args) {
-		System.out.println("Welcome To Employee Wage Computation Program");
-      
-		int IS_FULLTIME = 1;
-        int IS_PARTTIME = 2;
-        int Emp_Rate_PerHr = 20;
-       
-        int empHrs = 0;
-        int empWage = 0;
-        /*
-          Using Math.random method
-         */
-        Random random = new Random();
-        int empCheck = random.nextInt(3);
-        if (empCheck == IS_FULLTIME)
-            empHrs = 8;
-        else if (empCheck == IS_PARTTIME)
-            empHrs = 4;
-        else
-            empHrs = 0;
-        /*
-         Calculating the daily employee wage of an employee
-         */
-        empWage = Emp_Rate_PerHr * empHrs;
-        System.out.println("The daily wage of an employee is : " + empWage);
-    }
-}
+	    public static void main(String[] args) {
+	        System.out.println("Welcome To Employee Wage Computation Program");
+	        
+	        int empHrs = 0;
+	        int empWage = 0;
+	        /*
+	          Using random method 
+	         */
+	        Random random = new Random();
+	        int empCheck = random.nextInt(3);
+	        switch (empCheck) {
+	            case IS_FULLTIME: // FullTime Employee
+	                empHrs = 8;
+	                break;
+	            case IS_PARTTIME: // PartTime Employee
+	                empHrs = 4;
+	                break;
+	            default: // Employee is absent
+	                empHrs = 0;
+	        }
+	        /*
+	          Calculating the daily employee wage of an employee
+	         */
+	        empWage = Emp_Rate_PerHr * empHrs;
+	        System.out.println("The daily wage of an employee is : " + empWage);
+	    }
+	}
